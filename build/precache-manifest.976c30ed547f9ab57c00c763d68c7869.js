96ac98ea275afe46870568cb32f4f7bb6127bb77
Tong <chingtong289@gmail.com>
2024-08-17 23:37:20 +0700

    Initial commit

---
+++ build/precache-manifest.976c30ed547f9ab57c00c763d68c7869.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c64c5725cda49b2418ec1c3bdcf689f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5bde36509cd57302e66",
+    "url": "/static/css/main.4931c62b.chunk.css"
+  },
+  {
+    "revision": "6e558d8a8b4becca2c01",
+    "url": "/static/js/2.7ba7b443.chunk.js"
+  },
+  {
+    "revision": "a87d4d17df12822b7d1b66feae52f143",
+    "url": "/static/js/2.7ba7b443.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5bde36509cd57302e66",
+    "url": "/static/js/main.aa97e967.chunk.js"
+  },
+  {
+    "revision": "0b6b46ef1e17d32e6df4",
+    "url": "/static/js/runtime-main.bce919bb.js"
+  },
+  {
+    "revision": "1a585610b61d0a7b39014b233c266895",
+    "url": "/static/media/logo.1a585610.png"
+  }
+]);
